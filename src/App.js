f7b048217ecfb3437cf42888e58c15ef4bc45cd3
askuahwah <askuahwah@gmail.com>
2024-01-05 10:16:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Hooks from "./component/Hooks";
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <Hooks/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Hooks.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./hooks.css";
+
+const Hooks = () => {
+  return (
+    <div className="hooks_main">
+      <div>
+        <h1>Welcome in hooks</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Hooks;
